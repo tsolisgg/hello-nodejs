5eab1dee73a9bcd3cfe57ffa8da21412d1222a2b
tsolisgg <45158684+tsolisgg@users.noreply.github.com>
2021-09-05 00:29:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text\plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Habla Barrio');
 });
 
 server.listen(port, () => {
-  console.log('Server running on port: ${port}');
-  });
+  console.log(`Server running on port: ${port}`);
+});
